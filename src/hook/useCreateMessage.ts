ac5827fa5f22733628dda09ade29dceff68bd665
kaiwenxiao <617297811@qq.com>
2024-11-09 17:47:58 +0800

    chore: chat-item latest message subscription

--- src/hook/useCreateMessage.ts
+++ src/hook/useCreateMessage.ts
@@ -1,6 +1,7 @@
 import { useMutation } from "@apollo/client";
 import { graphql } from "../gql";
 import { updateMessages } from "../cache/messages.ts";
+import { updateLatestMessage } from "../cache/latest-message.ts";
 
 const createMessageDocument = graphql(`
   mutation CreateMessage($createMessageInput: CreateMessageInput!) {
@@ -33,6 +34,7 @@ const useCreateMessage = () => {
       //   2.
       if (data?.createMessage) {
         updateMessages(cache, data.createMessage);
+        updateLatestMessage(cache, data.createMessage);
       }
     },
   });
